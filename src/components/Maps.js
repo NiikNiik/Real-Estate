2fec1a611b76efe18738ecfa4bda3dc54801e0c5
Crispen LeBlanc <leblanc2042@gmail.com>
2024-11-12 14:14:28 -0700

    Changed Maps.js APIkey for googlemaps API. You must have a .env in your local folder with the variable called 'REACT_APP_GOOGLE_MAPS_API_KEY' and assign it to your google maps API key.

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -16,7 +16,7 @@ const MapComponent = ({ children }) => {
   };
 
   return (
-    <LoadScript googleMapsApiKey={process.env.local.REACT_APP_GOOGLE_MAPS_API_KEY}>
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         center={center}
